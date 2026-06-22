9056628381ba8d50fb40f29bc0876ef1e6d3739b
David Tamang <davidtamang.upwork@gmail.com>
2026-06-22 17:37:59 +0545

    updated seeder for permission

--- app/accounts/role_permission/seeders/seed_permissions.py
+++ app/accounts/role_permission/seeders/seed_permissions.py
@@ -4,31 +4,28 @@ import csv
 from pathlib import Path
 from sqlalchemy import select
 from core.database import AsyncSessionLocal
+from core.constants.permissions import ALL_PERMISSION_DETAILS
 from accounts.role_permission.infrastructure.orm import Permission
 
 async def seed_permissions():
-    file_path = Path(__file__).parent / "permissions.csv"
     async with AsyncSessionLocal() as session:
         try:
             stmt = select(Permission.codename)
             result = await session.execute(stmt)
             existing_permissions = result.scalars().all()
             new_permissions_to_create = []
-            with open(file_path, newline="") as file:
-                reader = csv.DictReader(file)
-                for row in reader:
-                    if row["codename"] not in existing_permissions:               
-                        new_permissions_to_create.append(
-                            Permission(
-                                codename=row["codename"],
-                                name=row["name"],
-                                is_super_admin_permission=row["is_super_admin_permission"] == "true",
-                                description=row["description"]
-                            )
+            for permission in ALL_PERMISSION_DETAILS:
+                if permission.codename not in existing_permissions:               
+                    new_permissions_to_create.append(
+                        Permission(
+                            codename=permission.codename,
+                            name=permission.name,
+                            description=permission.description
                         )
-                        logging.info(f"Adding permission {row["name"]} ...")
-                    else:
-                        logging.info(f"Skipping permission {row["name"]}, It already exists ...")
+                    )
+                    logging.info(f"Adding permission {permission.name} ...")
+                else:
+                    logging.info(f"Skipping permission {permission.name}, It already exists ...")
             if new_permissions_to_create:
                 session.add_all(new_permissions_to_create)
                 await session.commit()
