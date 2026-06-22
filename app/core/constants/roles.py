cb4d019919f70661761fd64cadecd5af819b0b65
David Tamang <davidtamang.upwork@gmail.com>
2026-06-22 17:39:32 +0545

    moved system roles constant here

--- app/core/constants/roles.py
+++ app/core/constants/roles.py
@@ -1,1 +1,16 @@
+from core.constants import permissions
+
 ADMIN = "ADMIN"
+
+STAFF_DEFAULT_ROLE = {
+    "name": ADMIN,
+    "is_system_role": True,
+    "permissions": permissions.STAFF_USER_PERMISSIONS
+}
+
+
+TENANT_DEFAULT_ROLE = {
+    "name": ADMIN,
+    "is_system_role": True,
+    "permissions": permissions.USER_PERMISSIONS
+}
