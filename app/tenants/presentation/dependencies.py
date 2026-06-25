f7b62f0bf0406c9c0c095c5dd74f89275b0d2c5f
David Tamang <davidtamang.upwork@gmail.com>
2026-06-25 11:49:47 +0545

    injected role permission service in tenant service object

--- app/tenants/presentation/dependencies.py
+++ app/tenants/presentation/dependencies.py
@@ -3,6 +3,7 @@ from core.unit_of_work import UnitOfWork
 from core.context import CurrentUser
 from core.dependencies import get_unit_of_work, get_current_user
 from tenants.application.services import TenantService
+from accounts.role_permission.application.services import RolePermissionService
 
 def get_tenant_service(
     uow: UnitOfWork = Depends(get_unit_of_work), 
@@ -11,7 +12,11 @@ def get_tenant_service(
 
     return TenantService(
         uow=uow,
-        current_user=current_user
+        current_user=current_user,
+        role_perm_service=RolePermissionService(
+            uow=uow, 
+            current_user=current_user
+        )
     )
 
 
