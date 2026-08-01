97016380457697383c62041ae0ba001231b0a4c1
David Tamang <davidtamang.upwork@gmail.com>
2026-08-01 09:59:31 +0545

    user domain model changes and new added

--- app/accounts/user/domain/models.py
+++ app/accounts/user/domain/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timezone
 from uuid import UUID, uuid4
-from pydantic import BaseModel, EmailStr, Field, ConfigDict
+from pydantic import EmailStr, Field, field_validator
 from accounts.user.domain.events import UserAddedToTenant, UserRegistered
 from core.events import BaseEvent
 from core.domain import AggregateRoot
@@ -8,28 +8,58 @@ from core.domain import AggregateRoot
 
 class User(AggregateRoot):
 
+    first_name: str
+    last_name: str
     email: EmailStr
-    id: UUID | None = Field(default_factory=uuid4)
-    is_staff: bool | None = False
-    is_active: bool | None = False
-    first_name: str | None = None
-    last_name: str | None = None
+    is_active: bool 
+    is_staff: bool
     verification_token: str | None = None
     verification_token_created_at: datetime | None = None
+    id: UUID | None = Field(default_factory=uuid4)
+
+
+    @staticmethod
+    def validate_name(value: str) -> str:
+        if not value:
+            raise ValueError("First name or last name should not be empty")
+        if len(value) > 125:
+            raise ValueError("First name or last name character length should not be greater than 125")
+        return value
+
+    @field_validator('first_name')
+    @classmethod
+    def validate_first_name(cls, value: str) -> str:
+        return cls.validate_name(value).capitalize()
+
+    @field_validator('last_name')
+    @classmethod
+    def validate_last_name(cls, value: str) -> str:
+        return cls.validate_name(value).capitalize()
 
     @classmethod
-    def create(cls, email: str, is_staff: bool, verification_token: str, verification_token_created_at: datetime) -> "User":
+    def create(
+        cls,
+        first_name: str,
+        last_name: str, 
+        email: str,
+        is_active: bool,
+        is_staff: bool, 
+        verification_token: str, 
+        verification_token_created_at: datetime
+    ) -> "User":
         user_id = uuid4()
 
         return cls(
             id=user_id,
+            first_name = first_name,
+            last_name = last_name,
             email = email,
             is_staff = is_staff,
             verification_token=verification_token,
             verification_token_created_at=verification_token_created_at,
             events = [
                 UserRegistered(
-                    email = email,
+                    send_to = email,
                     verification_token = verification_token
                 )
             ]
@@ -39,17 +69,21 @@ class UserTenant(AggregateRoot):
 
     email: EmailStr
     tenant_id: UUID
+    role_id: UUID
+    created_by_id: UUID | None = None
 
 
     @classmethod
-    def create(cls, email: str, tenant_id: UUID, tenant_name: str, tenant_code: str) -> "UserTenant":
+    def create(cls, email: str, role_id: UUID, tenant_id: UUID, tenant_name: str, tenant_code: str, created_by_id: UUID) -> "UserTenant":
 
         return cls(
             email = email,
             tenant_id = tenant_id,
+            role_id = role_id,
+            created_by_id = created_by_id,
             events = [
                 UserAddedToTenant(
-                    email = email,
+                    send_to = email,
                     tenant_name = tenant_name,
                     tenant_code = tenant_code
                 )
