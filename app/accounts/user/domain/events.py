8d9e19baed677ef55fed2dd4e72caf0f39bf1cea
David Tamang <davidtamang.upwork@gmail.com>
2026-09-04 13:38:03 +0545

    user events

--- app/accounts/user/domain/events.py
+++ app/accounts/user/domain/events.py
@@ -3,7 +3,7 @@ from core.events import BaseEvent, EventTypes
 
 
 @dataclass
-class UserRegistered(BaseEvent):
+class UserRegisteredEvent(BaseEvent):
 
     send_to: str
     verification_token: str
@@ -14,7 +14,7 @@ class UserRegistered(BaseEvent):
 
 
 @dataclass
-class UserAddedToTenant(BaseEvent):
+class UserAddedToTenantEvent(BaseEvent):
 
     send_to: str
     tenant_name: str
@@ -23,3 +23,64 @@ class UserAddedToTenant(BaseEvent):
     def __post_init__(self):
 
         self.event_type = EventTypes.USER_ADDED_TO_TENANT
+
+@dataclass
+class UserEmailChangeRequestEvent(BaseEvent):
+
+    send_to: list[str]
+    send_by: str
+    new_email: str
+    send_by_first_name: str
+    send_by_last_name: str
+    tenant_name: str | None = None
+    tenant_code: str | None = None
+    tenant_id: str | None = None
+
+    def __post_init__(self):
+
+        self.event_type = EventTypes.USER_EMAIL_CHANGE_REQUEST
+
+
+@dataclass
+class UserEmailChangeRequestApprovedEvent(BaseEvent):
+
+    send_to: str
+    approved_by_email: str
+    approved_by_first_name: str
+    approved_by_last_name: str
+    tenant_name: str | None = None
+    tenant_code: str | None = None
+    tenant_id: str | None = None
+
+    def __post_init__(self):
+
+        self.event_type = EventTypes.USER_EMAIL_CHANGE_REQUEST_APPROVED
+
+
+@dataclass
+class UserEmailChangeRequestRejectedEvent(BaseEvent):
+
+    send_to: str
+    rejected_by_email: str
+    rejected_by_first_name: str
+    rejected_by_last_name: str
+    tenant_name: str | None = None
+    tenant_code: str | None = None
+    tenant_id: str | None = None
+
+    def __post_init__(self):
+
+        self.event_type = EventTypes.USER_EMAIL_CHANGE_REQUEST_REJECTED
+
+
+@dataclass
+class UserVerifyEmailChangeEvent(BaseEvent):
+
+    send_to: str
+    tenant_name: str | None = None
+    tenant_code: str | None = None
+    tenant_id: str | None = None
+
+    def __post_init__(self):
+
+        self.event_type = EventTypes.USER_EMAIL_CHANGED
