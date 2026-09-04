e3cba821be69cb74d89f129638d91e0eca813afb
David Tamang <davidtamang.upwork@gmail.com>
2026-09-04 13:39:29 +0545

    user service dependencies updated

--- app/accounts/user/presentation/dependencies.py
+++ app/accounts/user/presentation/dependencies.py
@@ -1,12 +1,16 @@
 from typing import Annotated
 from fastapi import Depends
 from accounts.user.application.services import AbstractUserService, UserService
-from core.dependencies import get_current_user, get_unit_of_work, get_tenant_access_policy
+from core.dependencies import get_current_user, get_unit_of_work
 from core.unit_of_work import UnitOfWork
+from core.context import CurrentUser
+from core.security import PasswordHandler
+from accounts.role_permission.application.policies import TenantAccessPolicy
 
-def get_user_service(uow: Annotated[UnitOfWork, Depends(get_unit_of_work)]) -> AbstractUserService:
+def get_user_service(uow: Annotated[UnitOfWork, Depends(get_unit_of_work)], current_user: Annotated[CurrentUser, Depends(get_current_user)] ) -> AbstractUserService:
     return UserService(
         uow=uow,
-        tenant_access_policy=get_tenant_access_policy(),
-        current_user=get_current_user()
+        tenant_access_policy=TenantAccessPolicy(uow=uow, current_user=current_user),
+        current_user=current_user,
+        password_handler=PasswordHandler()
     )
