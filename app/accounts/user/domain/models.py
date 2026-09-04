81d2f394d33d1d8b6a069b717b7f08c8ab1160bc
David Tamang <davidtamang.upwork@gmail.com>
2026-09-04 13:38:19 +0545

    user model updated

--- app/accounts/user/domain/models.py
+++ app/accounts/user/domain/models.py
@@ -1,10 +1,13 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from uuid import UUID, uuid4
 from pydantic import EmailStr, Field, field_validator
-from accounts.user.domain.events import UserAddedToTenant, UserRegistered
+from accounts.user.domain.events import UserAddedToTenantEvent, UserRegisteredEvent, UserEmailChangeRequestEvent, UserEmailChangeRequestApprovedEvent, UserEmailChangeRequestRejectedEvent, UserVerifyEmailChangeEvent
 from core.events import BaseEvent
 from core.domain import AggregateRoot
+from accounts.user.domain.enumns.email_change_request_enum import EmailChangeRequestEnum
 
+
+EMAIL_CHANGE_REQUEST_TTL = timedelta(hours=48)
 
 class UserModel(AggregateRoot):
 
@@ -13,9 +16,12 @@ class UserModel(AggregateRoot):
     email: EmailStr
     is_active: bool 
     is_staff: bool
+    hashed_password: str | None = None
+    verified_at: datetime | None = None
     verification_token: str | None = None
     verification_token_created_at: datetime | None = None
     id: UUID | None = Field(default_factory=uuid4)
+    is_deleted: bool | None = False
 
 
     @staticmethod
@@ -39,15 +45,17 @@ class UserModel(AggregateRoot):
     @classmethod
     def create(
         cls,
+        user_id: UUID,
         first_name: str,
         last_name: str, 
+        hashed_password: str,
         email: str,
         is_active: bool,
-        is_staff: bool, 
+        is_staff: bool,
+        is_deleted: bool,
         verification_token: str, 
-        verification_token_created_at: datetime
+        verification_token_created_at: datetime,
     ) -> "User":
-        user_id = uuid4()
 
         return cls(
             id=user_id,
@@ -55,34 +63,180 @@ class UserModel(AggregateRoot):
             last_name = last_name,
             email = email,
             is_staff = is_staff,
+            is_active = is_active,
+            is_deleted = is_deleted,
             verification_token=verification_token,
             verification_token_created_at=verification_token_created_at,
+            hashed_password = hashed_password,
             events = [
-                UserRegistered(
+                UserRegisteredEvent(
                     send_to = email,
                     verification_token = verification_token
                 )
             ]
         )
 
+class EmailChangeRequestModel(AggregateRoot):
+
+    id: UUID
+    old_email: EmailStr
+    new_email: EmailStr
+    user_id: UUID
+    new_email_verification_token: str | None = None
+    new_email_verification_token_created_at: datetime | None = None
+    status: EmailChangeRequestEnum
+
+    @classmethod
+    def create(
+        cls, 
+        old_email: str, 
+        new_email: str,
+        user_id: UUID,
+        status: "EmailChangeRequestEnum",
+        send_to: list[str],
+        send_by_first_name: str | None = None,
+        send_by_last_name: str | None = None,
+        tenant_id: UUID | None = None,
+        tenant_name: str | None = None,
+        tenant_code: str | None = None
+    ) -> "EmailChangeRequestModel":
+
+        return cls(
+            id=uuid4(),
+            old_email=old_email,
+            new_email=new_email,
+            new_email_verification_token=new_email_verification_token,
+            new_email_verification_token_created_at=new_email_verification_token_created_at,
+            status=status,
+            events = [
+                UserEmailChangeRequestEvent(
+                    send_to=send_to,
+                    send_by=old_email,
+                    new_email=new_email,
+                    send_by_first_name=send_by_first_name,
+                    send_by_last_name=send_by_last_name,
+                    user_id=user_id,
+                    tenant_id=tenant_id,
+                    tenant_code=tenant_code,
+                    tenant_name=tenant_name
+                )
+            ]
+        )
+
+    @classmethod
+    def update(
+        cls,
+        email_change_request_id: UUID,
+        status: "EmailChangeRequestEnum",
+        send_to: str,
+        old_email: str,
+        new_email: str,
+        user_id: UUID,
+        send_by_first_name: str | None = None,
+        send_by_last_name: str | None = None,
+        new_email_verification_token: str | None = None, 
+        new_email_verification_token_created_at: datetime | None = None,
+        tenant_id: UUID | None = None,
+        tenant_name: str | None = None,
+        tenant_code: str | None = None,
+    ):
+        events = []
+
+        if status == EmailChangeRequestEnum.APPROVED:
+            events.append(
+                UserEmailChangeRequestApprovedEvent(
+                    send_to=send_to,
+                    approved_by_email=old_email,
+                    approved_by_first_name=send_by_first_name,
+                    approved_by_last_name=send_by_last_name,
+                    tenant_id=tenant_id,
+                    tenant_code=tenant_code,
+                    tenant_name=tenant_name
+                )
+            )
+
+        elif status == EmailChangeRequestEnum.REJECTED:
+            events.append(
+                UserEmailChangeRequestRejectedEvent(
+                    send_to=send_to,
+                    rejected_by_email=old_email,
+                    rejected_by_first_name=send_by_first_name,
+                    rejected_by_last_name=send_by_last_name,
+                    tenant_id=tenant_id,
+                    tenant_code=tenant_code,
+                    tenant_name=tenant_name
+                )
+            )
+
+        return cls(
+            id=email_change_request_id,
+            old_email=old_email,
+            new_email=new_email,
+            new_email_verification_token=new_email_verification_token,
+            new_email_verification_token_created_at=new_email_verification_token_created_at,
+            user_id=user_id,
+            status=status,
+            events = events
+        )
+
+
+    @classmethod
+    def verify(
+        cls,
+        email_change_request_id: UUID,
+        status: "EmailChangeRequestEnum",
+        send_to: str,
+        old_email: str,
+        new_email: str,
+        user_id: UUID,
+        new_email_verification_token: str | None = None, 
+        new_email_verification_token_created_at: datetime | None = None,
+    ) -> "EmailChangeRequestModel":
+
+        events = [
+            UserVerifyEmailChangeEvent(
+                send_to=send_to,
+            )
+        ]
+
+        return cls(
+            id=email_change_request_id,
+            old_email=old_email,
+            new_email=new_email,
+            new_email_verification_token=None,
+            new_email_verification_token_created_at=None,
+            user_id=user_id,
+            status=status,
+            events=events,
+        )
+
+
+
+    @property
+    def is_expired(self) -> bool:
+        return datetime.now(tz=timezone.utc) > self.new_email_verification_token_created_at + EMAIL_CHANGE_REQUEST_TTL
+
+
 class UserTenantModel(AggregateRoot):
 
-    email: EmailStr
-    tenant_id: UUID
     role_id: UUID
+    user_id: UUID
+    email: EmailStr | None = None
+    tenant_id: UUID | None = None
     created_by_id: UUID | None = None
 
 
     @classmethod
-    def create(cls, email: str, role_id: UUID, tenant_id: UUID, tenant_name: str, tenant_code: str, created_by_id: UUID) -> "UserTenant":
+    def create(cls, email: str, role_id: UUID, tenant_id: UUID, user_id:UUID, tenant_name: str, tenant_code: str, created_by_id: UUID) -> "UserTenantModel":
 
         return cls(
             email = email,
             tenant_id = tenant_id,
             role_id = role_id,
             created_by_id = created_by_id,
+            user_id = user_id,
             events = [
-                UserAddedToTenant(
+                UserAddedToTenantEvent(
                     send_to = email,
                     tenant_name = tenant_name,
                     tenant_code = tenant_code
@@ -90,4 +244,12 @@ class UserTenantModel(AggregateRoot):
             ]
         )
 
+    @classmethod
+    def create_for_staff(cls, role_id: UUID, user_id: UUID, created_by_id: UUID) -> "UserTenantModel":
+
+        return cls(
+            role_id = role_id,
+            user_id = user_id,
+            created_by_id = created_by_id
+        )
 
